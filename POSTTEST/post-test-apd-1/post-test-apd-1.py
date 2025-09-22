b27de3181d1ba83f785f61bac81f21816ab4d5bb
print_rizalll <muhammadfahrizaldani@gmail.com>
2025-09-23 00:56:58 +0800

    update

--- POSTTEST/post-test-apd-1/post-test-apd-1.py
+++ POSTTEST/post-test-apd-1/post-test-apd-1.py
@@ -1,11 +1,8 @@
-# Input nama dan nim
 nama = input("Masukan Nama Anda: ")
 nim = input("Masukan Nim Anda: ")
 
-# Input harga menu
 harga = int(input("Masukan harga makanan: "))
 
-# Data menu dan pajak
 menu = ["Pecel Lele", "Mie Ayam", "Nasi Padang"]
 persen_pajak = [5, 8, 10]
 
@@ -13,11 +10,9 @@ print("=" * 60)
 print(f"{nama} Dengan Nim {nim} ingin membeli makanan seharga Rp{harga:,}")
 print("=" * 60)
 
-# Header table
 print("{:<15} {:<10} {:<15}".format("Menu", "Pajak", "Total Harga"))
 print("-" * 60)
 
-# Loop hitung tiap menu
 for i in range(len(menu)):
     pajak = harga * (persen_pajak[i] / 100)
     total = harga + pajak
