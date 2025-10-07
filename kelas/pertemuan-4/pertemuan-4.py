5d9f35b91ff419d9eb9b22a46ccec1fd0dc74588
print_rizalll <muhammadfahrizaldani@gmail.com>
2025-10-07 23:27:26 +0800

    posttest-4

---
+++ kelas/pertemuan-4/pertemuan-4.py
@@ -0,0 +1,54 @@
+# for i in range(1, 10, 2):
+#     print(f"print ke-{i}")
+
+# mahasiswa = ["sahroni", "ahmad"]
+
+# for i in 123:
+#     print(i)
+
+# for i in range(1,10):
+#     for i in range(1,i+1):
+#         print("zuhri", end=" ")
+#     print("firza")
+
+# jawab = 'ya'
+# hitung = 0
+# while(jawab == 'ya'):
+#     hitung += 1
+#     jawab = input("Ulang lagi? ")
+# print(f"Total perulangan: {hitung}")
+
+# angka = [2, 5, 8, 12, 15, 7, 20]
+# print("Mencari angka pertama yang lebih besar dari 10...")
+# for n in angka:
+#     print(f"Sekarang memeriksa angka: {n}")
+#     if n > 10:
+#         print(f"Angka {n} lebih besar dari 10, Perulangan berhenti.")
+#         break
+# print("Program selesai.")
+
+# for i in range(1, 11):
+#     if i % 2 != 0:
+#         continue
+#         print(f"Angka genap ditemukan: {i}")
+# print("\nProgram selesai.")
+
+# panjang = int(input("Masukan Panjang Segitiga :"))
+
+# for i in range(1,panjang+1):
+#     for j in range(2*i-2):
+#         print(" ", end="")
+#     for j in range(2*i-2):w1zw
+#         print("*", end="")
+#     print("*")
+# print(" ")
+
+# print(" ")
+
+# for i in range(panjang,0,-1):
+#     for j in range(2*i-2):
+#         print(" ", end="")
+#     for j in range(2*i-2):
+#         print("*", end="")
+#     print("*")
+# print(" ")
